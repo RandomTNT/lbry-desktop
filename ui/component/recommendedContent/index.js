451a64b6fa86aae7d860242bbe576e3daacd7003
infinite-persistence <inf.persistence@gmail.com>
2021-03-19 23:04:12 +0800

    Fix autoplay in floating mode
    ## Issue
    Closes 5000: AutoPlay in Floating Player only works one time
    
    ## Changes
    Factor out the code that queries the recommendation. It needs to be called in two places:
    (1) `RecommendedContent` - need to cover the case of floating player + visit another video page.
    (2) When video is floating and autoplayed the next video.

--- ui/component/recommendedContent/index.js
+++ ui/component/recommendedContent/index.js
@@ -1,20 +1,19 @@
 import { connect } from 'react-redux';
-import { makeSelectClaimForUri, makeSelectClaimIsNsfw } from 'lbry-redux';
-import { doSearch } from 'redux/actions/search';
+import { makeSelectClaimIsNsfw } from 'lbry-redux';
+import { doGetRecommendedContent } from 'redux/actions/search';
 import { makeSelectRecommendedContentForUri, selectIsSearching } from 'redux/selectors/search';
 import { selectUserVerifiedEmail } from 'redux/selectors/user';
 import RecommendedVideos from './view';
 
 const select = (state, props) => ({
-  claim: makeSelectClaimForUri(props.uri)(state),
   mature: makeSelectClaimIsNsfw(props.uri)(state),
   recommendedContent: makeSelectRecommendedContentForUri(props.uri)(state),
   isSearching: selectIsSearching(state),
   isAuthenticated: selectUserVerifiedEmail(state),
 });
 
-const perform = dispatch => ({
-  search: (query, options) => dispatch(doSearch(query, options)),
+const perform = (dispatch) => ({
+  doGetRecommendedContent: (uri, mature) => dispatch(doGetRecommendedContent(uri, mature)),
 });
 
 export default connect(select, perform)(RecommendedVideos);
--- ui/component/recommendedContent/view.jsx
+++ ui/component/recommendedContent/view.jsx
@@ -6,46 +6,23 @@ import Ads from 'web/component/ads';
 import Card from 'component/common/card';
 import { useIsMobile, useIsMediumScreen } from 'effects/use-screensize';
 
-type Options = {
-  related_to: string,
-  nsfw?: boolean,
-};
-
 type Props = {
   uri: string,
-  claim: ?StreamClaim,
   recommendedContent: Array<string>,
   isSearching: boolean,
-  search: (string, Options) => void,
+  doGetRecommendedContent: (string, boolean) => void,
   mature: boolean,
   isAuthenticated: boolean,
 };
 
 export default function RecommendedContent(props: Props) {
-  const { uri, claim, search, mature, recommendedContent, isSearching, isAuthenticated } = props;
+  const { uri, doGetRecommendedContent, mature, recommendedContent, isSearching, isAuthenticated } = props;
   const isMobile = useIsMobile();
   const isMedium = useIsMediumScreen();
 
-  const stringifiedClaim = JSON.stringify(claim);
-  const getRecommendedContent = React.useCallback(() => {
-    if (stringifiedClaim) {
-      const jsonClaim = JSON.parse(stringifiedClaim);
-      if (jsonClaim && jsonClaim.value && jsonClaim.claim_id) {
-        const options: Options = { size: 20, related_to: jsonClaim.claim_id, isBackgroundSearch: true };
-        if (jsonClaim && !mature) {
-          options['nsfw'] = false;
-        }
-        const { title } = jsonClaim.value;
-        if (title && options) {
-          search(title, options);
-        }
-      }
-    }
-  }, [stringifiedClaim, mature]);
-
   React.useEffect(() => {
-    getRecommendedContent();
-  }, [uri]);
+    doGetRecommendedContent(uri, mature);
+  }, [uri, mature]);
 
   return (
     <Card
